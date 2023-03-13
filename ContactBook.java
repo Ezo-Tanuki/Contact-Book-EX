eabb8b569955f8e4862bc38912497c5bda8897b5
Ezo-Tanuki <michailbryanlie@gmail.com>
2023-03-13 19:50:43 +0700

    Remove package

--- ContactBook.java
+++ ContactBook.java
@@ -1,5 +1,3 @@
-package WEEK4;
-
 import java.util.LinkedList;
 import java.util.Scanner;
 
--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package WEEK4;
-
 public class Main {
     public static void main(String[] args) {
         new ContactBook().run();
